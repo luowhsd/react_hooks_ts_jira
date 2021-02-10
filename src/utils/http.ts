ca021d7fe0e050935ca5a9d1cb3f485649b5bfe7
luowhsd <591151328@qq.com>
2021-02-10 19:20:00 +0800

    feat: 用fetch抽象通用HTTP请求方法，增强通用性

---
+++ src/utils/http.ts
@@ -0,0 +1,42 @@
+import qs from "qs";
+import * as auth from "auth-provider";
+const apiUrl = process.env.REACT_APP_API_URL;
+interface Config extends RequestInit {
+  data?: object;
+  token?: string;
+}
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+  // axios 和 fetch 的表现不一样, axios在返回状态不为2xx的时候抛出异常
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        // 退出登录
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
